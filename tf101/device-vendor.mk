1fcad59786371c8df45486fa237767a272f891a8
Darren Smith <nerevar@gmail.com>
2018-12-18 14:50:16 -0600

    Correct file naming
    Change-Id: Iebb833d5e33a8179b48ff16735db108cfb6633b9

--- tf101/device-vendor.mk
+++ tf101/device-vendor.mk
@@ -17,4 +17,4 @@
 # Pick up overlay for features that depend on non-open-source files
 DEVICE_PACKAGE_OVERLAYS := vendor/asus/tf101/overlay
 
-$(call inherit-product, vendor/asus/tf101/tf101-vendor-blobs.mk)
+$(call inherit-product, vendor/asus/tf101/device-vendor-blobs.mk)
